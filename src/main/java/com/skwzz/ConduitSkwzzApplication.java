a44473a6a27f33c46c45c53794144d899fd40198
skwzz <kidol9108@gmail.com>
2024-07-23 23:22:38 +0900

    Initial commit

---
+++ src/main/java/com/skwzz/ConduitSkwzzApplication.java
@@ -0,0 +1,13 @@
+package com.skwzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitSkwzzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConduitSkwzzApplication.class, args);
+    }
+
+}
