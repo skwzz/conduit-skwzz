f953bc16e0a646d144f02af8ef403453449ec15c
KiwoongSon <kidol9108@smartscore.kr>
2024-07-30 15:47:14 +0900

    warning 코드 제거

--- src/main/java/com/skwzz/domain/user/controller/AuthController.java
+++ src/main/java/com/skwzz/domain/user/controller/AuthController.java
@@ -7,7 +7,6 @@ import com.skwzz.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
